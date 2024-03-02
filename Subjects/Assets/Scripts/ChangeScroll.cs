2b1520e0c1f57058a8f7d7347b75f616bceca01e
JasurGameMaker <anvarov.jasur19@gmail.com>
2024-03-02 10:24:59 +0500

    Camera To Prefab

--- Subjects/Assets/Scripts/ChangeScroll.cs
+++ Subjects/Assets/Scripts/ChangeScroll.cs
@@ -18,16 +18,46 @@ public class ChangeScroll : MonoBehaviour
     public string[] itemNames;
 
 
-    void Start()
+    bool isSnapped = false;
+
+
+
+    public float snapForce;
+
+    float snapSpeed;
+
+    private void Start()
     {
-        
+        isSnapped = false;
     }
 
 
     void Update()
     {
         int currentItem = Mathf.RoundToInt((0 - contentPanel.localPosition.x / (sampleListItem.rect.width + HLG.spacing)));
-
         print(currentItem);
+
+        if (scrollRect.velocity.magnitude < 200 && !isSnapped)
+        {
+
+            scrollRect.velocity = Vector2.zero;
+            snapSpeed += snapForce * Time.deltaTime;
+            contentPanel.localPosition = new Vector3(
+                Mathf.MoveTowards(contentPanel.localPosition.x, 0 - (currentItem * (sampleListItem.rect.width + HLG.spacing)), snapSpeed),
+                contentPanel.localPosition.y, 
+                contentPanel.localPosition.z);
+
+            if (contentPanel.localPosition.x == 0 - (currentItem * (sampleListItem.rect.width + HLG.spacing)))
+            {
+                isSnapped = true;
+            }
+
+        }
+
+        if (scrollRect.velocity.magnitude > 200)
+        {
+            isSnapped = false;
+            snapSpeed = 0;
+        }
     }
 }
