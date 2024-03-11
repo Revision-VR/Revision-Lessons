af948dade257b6590ace06b1d1085e267072d899
Nabiddinov <nabiddinovabror8@gmail.com>
2024-03-11 15:22:20 +0500

    update language all

--- Subjects/Assets/Scripts/Astronomiya/ButtonManager.cs
+++ Subjects/Assets/Scripts/Astronomiya/ButtonManager.cs
@@ -3,9 +3,24 @@ using UnityEngine;
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField] private string _info;
+    [SerializeField] private string _infoRu;
+    [SerializeField] private string _infoEn;
+
     public string newLayerName = "Outlined";
 
     private static GameObject selectedObject;
+    private string _language;
+
+    private void Start()
+    {
+        _language = PlayerPrefs.GetString("Language");
+        //if (string.IsNullOrEmpty(_language))
+        //{
+        //    PlayerPrefs.SetString("Language", "en");
+        //}
+            PlayerPrefs.SetString("Language", "en");
+
+    }
 
     public void ForButton()
     {
@@ -16,6 +31,20 @@ public class ButtonManager : MonoBehaviour
 
         selectedObject = gameObject;
 
+        //switch (_language)
+        //{
+        //    case "uz":
+        //        TextManager.Instance.ChangableInfo.text = _info;
+        //        break;
+        //    case "ru":
+        //        TextManager.Instance.ChangableInfo.text = _infoRu;
+        //        break;
+
+        //    case "en":
+        //        TextManager.Instance.ChangableInfo.text = _infoEn;
+        //        break;
+        //}
+
         TextManager.Instance.ChangableInfo.text = _info;
         int newLayer = LayerMask.NameToLayer(newLayerName);
         gameObject.layer = newLayer;
@@ -23,8 +52,6 @@ public class ButtonManager : MonoBehaviour
 
     private void OnMouseDown()
     {
-       
-
         ForButton();
     }
 
