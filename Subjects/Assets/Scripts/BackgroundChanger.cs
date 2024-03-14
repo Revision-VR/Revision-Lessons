8751b6a1eed4da4bb5c85cffe652b94aa761dd61
Nabiddinov <nabiddinovabror8@gmail.com>
2024-03-14 13:12:01 +0500

    Add Actinum theme

--- Subjects/Assets/Scripts/BackgroundChanger.cs
+++ Subjects/Assets/Scripts/BackgroundChanger.cs
@@ -4,13 +4,13 @@ using UnityEngine.UI;
 public class BackgroundChanger : MonoBehaviour
 {
     public RawImage backgroundImage;
-    public Texture[] backgrounds; 
+    public Texture[] backgrounds;
 
     public void ChangeBackground(int backgroundIndex)
     {
         if (backgroundIndex >= 0 && backgroundIndex < backgrounds.Length)
         {
-            backgroundImage.texture = backgrounds[backgroundIndex]; 
+            backgroundImage.texture = backgrounds[backgroundIndex];
         }
         else
         {
