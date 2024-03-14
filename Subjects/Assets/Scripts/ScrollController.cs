953594dfff160de91fd47d16c2944b76d1066acc
Nabiddinov <nabiddinovabror8@gmail.com>
2024-03-14 16:10:37 +0500

    update

--- Subjects/Assets/Scripts/ScrollController.cs
+++ Subjects/Assets/Scripts/ScrollController.cs
@@ -1,29 +1,24 @@
-using System;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class ScrollController : MonoBehaviour
 {
     [SerializeField]
     private GameObject GameObject;
-    private int count = 1;
 
-   
+    [SerializeField]
+    private GameObject[] GameObjectsFalse;
+
+    public void SetActiveFalse()
+    {
+        foreach (GameObject obj in GameObjectsFalse)
+        {
+                obj.SetActive(false);
+        }
+    }
+
+
     public void OnClick()
     {
-        //if(count % 2 != 0)
-        //{
-        //    gameObject.SetActive(true);
-        //    Debug.Log("true");
-        //    count++;
-        //}
-        //else if (count % 2 == 0)
-        //{
-        //    gameObject.SetActive(false);
-        //    Debug.Log("false");
-        //    count++;
-        //}
-
         GameObject.SetActive(!GameObject.activeSelf);
     }
 }
