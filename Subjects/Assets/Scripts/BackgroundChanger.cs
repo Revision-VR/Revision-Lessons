2d0d855574bf91fa41d657a127791523feec0f6d
Nabiddinov <nabiddinovabror8@gmail.com>
2024-03-14 11:38:41 +0500

    add BG

---
+++ Subjects/Assets/Scripts/BackgroundChanger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundChanger : MonoBehaviour
+{
+    public RawImage backgroundImage;
+    public Texture[] backgrounds; 
+
+    public void ChangeBackground(int backgroundIndex)
+    {
+        if (backgroundIndex >= 0 && backgroundIndex < backgrounds.Length)
+        {
+            backgroundImage.texture = backgrounds[backgroundIndex]; 
+        }
+        else
+        {
+            Debug.LogError("Invalid background index!");
+        }
+    }
+}
